9ff9587ad570c731ae57971fba0605f7350059d9
yousef1712 <yousef.mohd2007y@gmail.com>
2023-08-13 15:57:33 +0300

    make the move to the login page

--- app/src/main/java/com/example/engineeringvillageproject/MainActivity.java
+++ app/src/main/java/com/example/engineeringvillageproject/MainActivity.java
@@ -4,12 +4,17 @@ package com.example.engineeringvillageproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout startLoginPage, loginPage;
     Button btLoginStart, btLogin;
+    Intent i;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -19,5 +24,21 @@ public class MainActivity extends AppCompatActivity {
 
         btLoginStart = findViewById(R.id.btLoginStart);
         btLogin = findViewById(R.id.btLogin);
+        startLoginPage = findViewById(R.id.startLoginPage);
+        loginPage = findViewById(R.id.loginPage);
+        i = new Intent(MainActivity.this, LoginActivity.class);
+
+        btLoginStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = startLoginPage.getVisibility();
+                int y = loginPage.getVisibility();
+                if (x == View.VISIBLE && y == View.GONE){
+                    startLoginPage.setVisibility(View.GONE);
+                    loginPage.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
     }
 }

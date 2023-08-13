fc6b67ecf56a319d425df4348d5dc106d2ad2062
yousef1712 <yousef.mohd2007y@gmail.com>
2023-08-13 14:24:06 +0300

    add the base xml and java

--- app/src/main/java/com/example/engineeringvillageproject/MainActivity.java
+++ app/src/main/java/com/example/engineeringvillageproject/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.engineeringvillageproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btLoginStart, btLogin;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btLoginStart = findViewById(R.id.btLoginStart);
+        btLogin = findViewById(R.id.btLogin);
     }
 }

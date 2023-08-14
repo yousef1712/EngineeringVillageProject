d3f8dca2a9555a4bf777f93bb0a6ca15b67ecf75
yousef1712 <yousef.mohd2007y@gmail.com>
2023-08-14 12:56:16 +0300

    add the back and next in the einginring club activity

--- app/src/main/java/com/example/engineeringvillageproject/EngineeringClubActivity.java
+++ app/src/main/java/com/example/engineeringvillageproject/EngineeringClubActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 public class EngineeringClubActivity extends AppCompatActivity {
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

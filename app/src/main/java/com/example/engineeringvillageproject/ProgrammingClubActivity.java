7a491c19c504a9eaaedaabd7b867fd77b9f0f221
yousef1712 <yousef.mohd2007y@gmail.com>
2023-08-15 12:42:04 +0300

    programe the buttons next, back and back to clubs

--- app/src/main/java/com/example/engineeringvillageproject/ProgrammingClubActivity.java
+++ app/src/main/java/com/example/engineeringvillageproject/ProgrammingClubActivity.java
@@ -2,13 +2,57 @@ package com.example.engineeringvillageproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class ProgrammingClubActivity extends AppCompatActivity {
 
+    LinearLayout howWe, theOtherThing;
+    Button btBack, btBackToClubs, btNext;
+    Intent i;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_programming_club);
+
+        howWe = findViewById(R.id.howWe);
+        theOtherThing = findViewById(R.id.theOtherThing);
+        btBack = findViewById(R.id.btBack);
+        btBackToClubs = findViewById(R.id.btBackToClubs);
+        btNext = findViewById(R.id.btNext);
+        i = new Intent(ProgrammingClubActivity.this, LoginActivity.class);
+
+        btNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                howWe.setVisibility(View.GONE);
+                theOtherThing.setVisibility(View.VISIBLE);
+                btBackToClubs.setVisibility(View.GONE);
+                btBack.setVisibility(View.VISIBLE);
+                btNext.setVisibility(View.GONE);
+            }
+        });
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                howWe.setVisibility(View.VISIBLE);
+                theOtherThing.setVisibility(View.GONE);
+                btBackToClubs.setVisibility(View.VISIBLE);
+                btNext.setVisibility(View.VISIBLE);
+                btBack.setVisibility(View.GONE);
+            }
+        });
+        btBackToClubs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
     }
 }
